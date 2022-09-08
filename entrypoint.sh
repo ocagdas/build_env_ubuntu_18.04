b9c98baabfa238c10e587d78f7ad6dd1c28763ee
h.ozgur cagdas <ocagdas@zmartsolutions.com>
2022-09-08 14:35:27 +0100

    install the gosu package via apt rather than downloading manually and make the necessary changes to point at the new binary location. Also install git and u-boot-tools

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
  
-# If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 9999 as default
-CURRENT_UID=${uid:-9999}
+# If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 1000 as default
+CURRENT_UID=${uid:-1000}
  
 # Notify user about the UID selected
 echo "Current UID : $CURRENT_UID"
@@ -12,7 +12,9 @@ passwd -d docker
 # Set "HOME" ENV variable for user's home directory
 export HOME=/home/docker
 echo "docker  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers; echo "Defaults    !requiretty" >> /etc/sudoers
- 
-# Execute process
-exec /usr/local/bin/gosu docker "$@"
 
+#echo "alias grep='grep -a'" >> /home/docker/.bashrc
+
+# Execute process
+#exec /usr/local/bin/gosu docker "$@"
+exec gosu docker "$@"
